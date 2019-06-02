8b1198559a560985e46866cc180b025acd3415ba
Damian Centurion <damicentu@gmail.com>
2019-06-01 21:38:12 -0300

    Add first level, player flipbooks and enemies sprites

---
+++ Source/DamianCenturionTP2/MushroomEnemy.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperFlipbookActor.h"
+#include "MushroomEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DAMIANCENTURIONTP2_API AMushroomEnemy : public APaperFlipbookActor
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
